68053a0240ac783afc1b104ef182aafcc3ea35ff
Šimek Pavel <pavel.simek@gopass.travel>
2026-02-17 14:58:49 +0100

    Reset migrations - single head marker since DB already has all tables

---
+++ apps/api/migrations/versions/head.py
@@ -0,0 +1,22 @@
+"""Initial schema state
+
+Revision ID: head
+Revises:
+Create Date: 2026-02-17 14:40:00
+
+"""
+
+from alembic import op
+
+revision = "head"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
