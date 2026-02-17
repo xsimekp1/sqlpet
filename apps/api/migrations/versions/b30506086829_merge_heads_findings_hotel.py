8cb0f68d06026f1488236d5a8f9af98d8c779522
Šimek Pavel <pavel.simek@gopass.travel>
2026-02-17 12:47:43 +0100

    fix: alembic merge migration and hardcode admin as superadmin

--- apps/api/migrations/versions/b30506086829_merge_heads_findings_hotel.py
+++ apps/api/migrations/versions/b30506086829_merge_heads_findings_hotel.py
@@ -1,10 +1,11 @@
 """merge_heads_findings_hotel
 
 Revision ID: b30506086829
-Revises: 9409224bccf4, add_hotel_with_owner
+Revises: add_intake_outcome_dates, add_hotel_with_owner_status
 Create Date: 2026-02-16 18:45:24.018336
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -12,8 +13,11 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'b30506086829'
-down_revision: Union[str, Sequence[str], None] = ('9409224bccf4', 'add_hotel_with_owner')
+revision: str = "b30506086829"
+down_revision: Union[str, Sequence[str], None] = (
+    "add_intake_outcome_dates",
+    "add_hotel_with_owner_status",
+)
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
