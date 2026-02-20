5b9ba257a8fa30a9af38a5203c49f037999a29d9
Šimek Pavel <pavel.simek@gopass.travel>
2026-02-20 16:34:59 +0100

    fix: add missing with_owner enum value to animal_status
    This fixes failing tests that were trying to set animal status to 'with_owner' but the enum value didn't exist in the database.

---
+++ apps/api/migrations/versions/b36400f933ef_add_with_owner_to_animal_status_enum.py
@@ -0,0 +1,29 @@
+"""add_with_owner_to_animal_status_enum
+
+Revision ID: b36400f933ef
+Revises: 845d0e8ad167
+Create Date: 2026-02-20 16:34:26.349314
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "b36400f933ef"
+down_revision: Union[str, Sequence[str], None] = "845d0e8ad167"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.execute("ALTER TYPE animal_status_enum ADD VALUE 'with_owner'")
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
